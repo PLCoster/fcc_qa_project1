9e00a1802e15276f9431959789969de82a6384fc
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-31 23:50:55 -0700

    Added an initial api route test for GET to / -> returns index.html
    All route tests currently passing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,19 +1,37 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
-let assert = chai.assert;
+const assert = chai.assert;
 const server = require('../server');
+const fs = require('fs');
+const path = require('path');
 
 // chai-http used for testing api routes
 chai.use(chaiHttp);
 
-suite('Functional Tests', function (done) {
-  test('Server sends index.html page on GET request to /', () => {
+suite('Functional Tests', function () {
+  test('Server sends index.html page on GET request to /', (done) => {
     chai
       .request(server)
       .get('/')
-      .then((data) => {
-        console.log('DATA IS: ', data);
+      .then((res) => {
+        assert.equal(res.status, 200, 'GET / Response status should be 200');
+        assert.equal(res.type, 'text/html');
+        assert.equal(
+          res.text,
+          fs.readFileSync(path.join(__dirname, '../views/index.html'), {
+            encoding: 'utf8',
+            flag: 'r',
+          }),
+          'Response text should be index.html',
+        );
         done();
       });
   });
+
+  // TESTS TO DO:
+  // Convert a valid input such as 10L: GET request to /api/convert.
+  // Convert an invalid input such as 32g: GET request to /api/convert.
+  // Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
+  // Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
+  // Convert with no number such as kg: GET request to /api/convert.
 });
