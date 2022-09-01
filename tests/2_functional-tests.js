e63595ed7a59df66281bf21cf0e926f9488e066a
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-31 23:48:20 -0700

    Updated package json test script to auto-exit test suite after running

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -3,8 +3,17 @@ const chai = require('chai');
 let assert = chai.assert;
 const server = require('../server');
 
+// chai-http used for testing api routes
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite('Functional Tests', function (done) {
+  test('Server sends index.html page on GET request to /', () => {
+    chai
+      .request(server)
+      .get('/')
+      .then((data) => {
+        console.log('DATA IS: ', data);
+        done();
+      });
+  });
 });
