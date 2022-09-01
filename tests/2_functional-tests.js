cc691f55832a4625de1d8f61f975f22496ab14fb
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-01 13:40:38 -0700

    Added tests for '/' and /api/convert server routes, with valid and invalid inputs

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -28,10 +28,125 @@ suite('Functional Tests', function () {
       });
   });
 
-  // TESTS TO DO:
-  // Convert a valid input such as 10L: GET request to /api/convert.
-  // Convert an invalid input such as 32g: GET request to /api/convert.
-  // Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
-  // Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
-  // Convert with no number such as kg: GET request to /api/convert.
+  test('Successfully converts on valid input to /api/convert (10L)', (done) => {
+    const expectedResult = {
+      initNum: 10,
+      initUnit: 'L',
+      returnNum: 2.64172,
+      returnUnit: 'gal',
+      string: '10 liters converts to 2.64172 gallons',
+    };
+
+    chai
+      .request(server)
+      .get('/api/convert?input=10L')
+      .then((res) => {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(
+          res.type,
+          'application/json',
+          'Response type should be application/json',
+        );
+        assert.deepEqual(
+          res.body,
+          expectedResult,
+          'Returned JSON should have expected keys and values for successful conversion',
+        );
+        done();
+      });
+  });
+
+  test('Successfully converts on valid unit-only input to /api/convert (kg)', (done) => {
+    const expectedResult = {
+      initNum: 1,
+      initUnit: 'kg',
+      returnNum: 2.20462,
+      returnUnit: 'lbs',
+      string: '1 kilograms converts to 2.20462 pounds',
+    };
+
+    chai
+      .request(server)
+      .get('/api/convert?input=kg')
+      .then((res) => {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(
+          res.type,
+          'application/json',
+          'Response type should be application/json',
+        );
+        assert.deepEqual(
+          res.body,
+          expectedResult,
+          'Returned JSON should have expected keys and values for successful conversion',
+        );
+        done();
+      });
+  });
+
+  test('Returns error on invalid unit input to /api/convert (32kilomegagram)', (done) => {
+    const expectedResult = 'invalid unit';
+
+    chai
+      .request(server)
+      .get('/api/convert?input=32kilomegagram')
+      .then((res) => {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(
+          res.type,
+          'application/json',
+          'Response type should be application/json',
+        );
+        assert.deepEqual(
+          res.body,
+          expectedResult,
+          'Returned JSON should have expected error message for invalid unit input',
+        );
+        done();
+      });
+  });
+
+  test('Returns error on invalid number input to /api/convert (3/7.2/3kg)', (done) => {
+    const expectedResult = 'invalid number';
+
+    chai
+      .request(server)
+      .get('/api/convert?input=3/7.2/3kg')
+      .then((res) => {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(
+          res.type,
+          'application/json',
+          'Response type should be application/json',
+        );
+        assert.deepEqual(
+          res.body,
+          expectedResult,
+          'Returned JSON should have expected error message for invalid number input',
+        );
+        done();
+      });
+  });
+
+  test('Returns error on invalid number and unit input to /api/convert (3/7.2/3kilomegagram)', (done) => {
+    const expectedResult = 'invalid number and unit';
+
+    chai
+      .request(server)
+      .get('/api/convert?input=3/7.2/3kilomegagram')
+      .then((res) => {
+        assert.equal(res.status, 200, 'Response status should be 200');
+        assert.equal(
+          res.type,
+          'application/json',
+          'Response type should be application/json',
+        );
+        assert.deepEqual(
+          res.body,
+          expectedResult,
+          'Returned JSON should have expected error message for invalid number and unit input',
+        );
+        done();
+      });
+  });
 });
